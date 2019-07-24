b5a68ffb0134dbf1de026a8b864fc539fb7883d0
V@no <git@vano.org>
2019-07-23 21:15:47 -0400

    v2.2 (2019-07-23)
    + option to focus on messages list

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -8,6 +8,7 @@ var self = this,
 		pref = Services.prefs.getBranch(PREF_BRANCH),
 		prefs = {
 			sel: 1,
+			focus: true
 		},
 		log = console.log;
 
@@ -55,8 +56,8 @@ function main(window)
 				if (!window.gFolderDisplay.navigate(msg, /* select */ true) && msg != msgDefault)
 					window.gFolderDisplay.navigate(msgDefault, /* select */ true)
 
-					if (!this.isTextbox(window.document.activeElement))
-						window.gFolderDisplay.tree.focus();
+					if (pref.getBoolPref("focus") && !this.isTextbox(window.document.activeElement))
+						window.gFolderDisplay.tree.focus()
 			}
 		},
 
@@ -91,7 +92,7 @@ function main(window)
 	
 function disableAll(obj, r)
 {
-	if (obj.tagName == "button" || obj.id == "autoSLM_box")
+	if (obj.tagName == "button" || obj.id.match(/^autoSLM/))
 		return;
 
 	if (!r && !obj.hasAttribute("backupDisabled"))
@@ -110,6 +111,88 @@ function disableAll(obj, r)
 		}
 	}
 }
+
+function prefString(pref, key, val)
+{
+	let r, er = [];
+	if (typeof(val) == "undefined")
+	{
+		try
+		{
+			r = pref.getComplexValue(key, Ci.nsISupportsString).data;
+		}
+		catch(e)
+		{
+			er.push(e);
+			try
+			{
+				r = pref.getStringPref(key);
+			}
+			catch(e)
+			{
+				er.push(e);
+				try
+				{
+					r = pref.getComplexValue(key, Ci.nsIPrefLocalizedString).data;
+				}
+				catch(e)
+				{
+					er.push(e);
+					try
+					{
+						r = pref.getCharPref(key);
+					}
+					catch(e)
+					{
+						er.push(e);
+						log(er);
+					}
+				}
+			}
+		}
+	}
+	else
+	{
+		try
+		{
+			let str = Cc["@mozilla.org/supports-string;1"].createInstance(Ci.nsISupportsString);
+			str.data = val;
+			r = pref.setComplexValue(key, Ci.nsISupportsString, str);
+		}
+		catch(e)
+		{
+			er.push(e);
+			try
+			{
+				r = pref.setStringPref(key,val);
+			}
+			catch(e)
+			{
+				er.push(e);
+				try
+				{
+					let str = Cc["@mozilla.org/pref-localizedstring;1"].createInstance(Ci.nsIPrefLocalizedString);
+					str.data = val;
+					r = pref.setComplexValue(key, Ci.nsIPrefLocalizedString, str);
+				}
+				catch(e)
+				{
+					er.push(e);
+					try
+					{
+						r = pref.setCharPref(key, val);
+					}
+					catch(e)
+					{
+						er.push(e);
+						log(er);
+					}
+				}
+			}
+		}
+	}
+	return r;
+}//prefString()
 
 function setDefaultPrefs(prefs, prefix)
 {
@@ -139,8 +222,8 @@ function setDefaultPrefs(prefs, prefix)
 				val = pref.getIntPref(name + key);
 				break;
 			case "string":
-				branch.setCharPref(name + key, val);
-				val = pref.getCharPref(name + key);
+				prefString(branch, name + key, val);
+				val = prefString(pref, name + key, val);
 				break;
 			case "object":
 				prefs[val] = setDefaultPrefs(prefs, key);
@@ -162,23 +245,43 @@ function fixpref(window, r, s)
 		return;
 
 	r =  r ? true : false;
+
 	if (doc.getElementById("mailnewsStartPageEnabled"))
 	{
+		function addElement(el, parent, type)
+		{
+			type = type || "appendChild";
+
+			if (type == "insertBefore")
+				parent.parentNode.insertBefore(el, parent);
+			else
+				parent[type](el);
+
+			if (!r)
+			{
+				listen(window, window, "unload", unload(function()
+				{
+					el.parentNode.removeChild(el);
+				}), false);
+			}
+		}
 		if (!r && !doc.getElementById("autoSLM_box"))
 		{
-			let h = doc.getElementById("mailnewsStartPageEnabled").parentNode.parentNode.clientHeight;
-			let w = doc.getElementById("mailnewsStartPageEnabled").parentNode.parentNode.clientWidth;
+			let h = doc.getElementById("mailnewsStartPageEnabled").parentNode.parentNode.clientHeight,
+					w = doc.getElementById("mailnewsStartPageEnabled").parentNode.parentNode.clientWidth,
+					checkbox = doc.createElement("checkbox"),
+					menupopup = doc.createElement("menupopup"),
+					menulist = doc.createElement("menulist"),
+					box = doc.createElement("hbox"),
+					menuitem = doc.createElement("menuitem"),
+					p = doc.createElement("preference");
 
-			let menupopup = doc.createElement("menupopup")
-			let menulist = doc.createElement("menulist");
 			menulist.id = "autoSLM";
 			menulist.setAttribute("label", "Select first unread message only");
-			menulist.setAttribute("preference", "autoSLM_pref");
+			menulist.setAttribute("preference", PREF_BRANCH + "sel");
 			menulist.addEventListener("command", prefChange, true);
-			let box = doc.createElement("hbox");
 			box.setAttribute("flex", false);
 			box.id = "autoSLM_box";
-			let menuitem = doc.createElement("menuitem");
 			menuitem.setAttribute("value", 0);
 			menuitem.setAttribute("label", "Default");
 			menupopup.appendChild(menuitem);
@@ -192,27 +295,36 @@ function fixpref(window, r, s)
 			menupopup.appendChild(menuitem);
 			menulist.appendChild(menupopup);
 			box.appendChild(menulist);
-			
-			doc.getElementById("mailnewsStartPageEnabled").parentNode.parentNode.insertBefore(box, doc.getElementById("mailnewsStartPageEnabled").parentNode);
 
-			let p = doc.createElement("preference");
-			p.id = "autoSLM_pref";
+			addElement(box, doc.getElementById("mailnewsStartPageEnabled").parentNode, "insertBefore");
+
+			checkbox.id = "autoSLM_checkbox";
+			checkbox.setAttribute("label", "Auto focus on messages list");
+			checkbox.setAttribute("preference", PREF_BRANCH + "focus");
+			checkbox.addEventListener("command", prefChange, true);
+			addElement(checkbox, box);
+
+			p.id = PREF_BRANCH + "sel";
 			p.setAttribute("type", "int");
-			p.name = "extensions.autoselectlatestmessage.sel";
-			doc.getElementById("generalPreferences").appendChild(p);
+			p.setAttribute("name", p.id);
+			addElement(p, doc.getElementById("generalPreferences"));
 
+			p = doc.createElement("preference");
+			p.id = PREF_BRANCH + "focus";
+			p.setAttribute("type", "bool");
+			p.setAttribute("name", p.id);
+			addElement(p, doc.getElementById("generalPreferences"));
+			
 			disableAll(doc.getElementById("mailnewsStartPageEnabled").parentNode.parentNode, menulist.value == 0);
 			width = doc.getElementById("mailnewsStartPageEnabled").parentNode.parentNode.clientWidth - w;
 			height = doc.getElementById("mailnewsStartPageEnabled").parentNode.parentNode.clientHeight - h;
-			window.resizeBy(width, height);
+//			window.resizeBy(width, height);
 			doc.getElementById("MailPreferences").showPane(doc.getElementById("MailPreferences").currentPane);
 		}
 		if (!r)
 			listen(window, window, "unload", unload(function(){
-				doc.getElementById("autoSLM_box").parentNode.removeChild(doc.getElementById("autoSLM_box"));
-				doc.getElementById("autoSLM_pref").parentNode.removeChild(doc.getElementById("autoSLM_pref"));
 				disableAll(doc.getElementById("mailnewsStartPageEnabled").parentNode.parentNode, true);
-				window.resizeBy(-width, -height);
+//				window.resizeBy(-width, -height);
 			}), false);
 	}
 	else if (!s && doc.getElementById("paneGeneral"))
