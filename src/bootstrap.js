5cb86bd703510be5ec1ecea99c4a525ca428cc70
V@no <git@vano.org>
2020-10-25 17:11:17 -0400

    v3.0.0 (2020-10-25)
    Adopted for Thunderbird 78

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,7 +1,6 @@
 const {classes: Cc, interfaces: Ci, utils: Cu} = Components,
 			PREF_BRANCH = "extensions.autoselectlatestmessage.";
-Cu.import("resource://gre/modules/Services.jsm");
-Cu.import("resource://gre/modules/AddonManager.jsm");
+
 var self = this,
 		width = 0,
 		height = 0,
@@ -10,7 +9,8 @@ var self = this,
 			sel: 1,
 			focus: true
 		},
-		log = console.log;
+		log = console.log.bind(console);
+
 
 function include(path)
 {
@@ -19,20 +19,24 @@ function include(path)
 
 function main(window)
 {
+
 	if (!"FolderDisplayListenerManager" in window)
 		return;
 
-	let func = {
-		selectMessageDelayed: function()
+	let document = window.document,
+			FolderDisplayListenerManager_listener = {
+
+		selectMessageDelayed: function FolderDisplayListenerManager_listener_selectMessageDelayed()
 		{
 			//the timer needed to allow time to restore previous selection if any
-			Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer).init({observe: function()
+			Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer).init({observe: function timer_observer()
 				{
-					func.selectMessage();
+					FolderDisplayListenerManager_listener.selectMessage();
 				}
 			}, 100, Ci.nsITimer.TYPE_ONE_SHOT);
 		},
-		selectMessage: function()
+
+		selectMessage: function FolderDisplayListenerManager_listener_selectMessage()
 		{
 			var sel = pref.getIntPref("sel");
 			if (!sel)
@@ -41,8 +45,8 @@ function main(window)
 			if (window.gFolderDisplay && window.gFolderDisplay.view.dbView && window.gFolderDisplay.view.dbView.numSelected == 0)
 			{
 				let msgDefault = (window.gFolderDisplay.view.isSortedAscending && window.gFolderDisplay.view.sortImpliesTemporalOrdering)
-									? window.nsMsgNavigationType.lastMessage
-									: window.nsMsgNavigationType.firstMessage;
+									? Ci.nsMsgNavigationType.lastMessage
+									: Ci.nsMsgNavigationType.firstMessage;
 				switch(sel)
 				{
 					case 1:
@@ -50,7 +54,7 @@ function main(window)
 					default:
 						break;
 					case 2:
-							msg = window.nsMsgNavigationType.firstUnreadMessage;
+							msg = Ci.nsMsgNavigationType.firstUnreadMessage;
 						break;
 				}
 				if (!window.gFolderDisplay.navigate(msg, /* select */ true) && msg != msgDefault)
@@ -61,7 +65,7 @@ function main(window)
 			}
 		},
 
-		isTextbox: function(el)
+		isTextbox: function FolderDisplayListenerManager_listener_isTextbox(el)
 		{
 			if (!el)
 				return false;
@@ -72,42 +76,106 @@ function main(window)
 			return this.isTextbox(el.parentNode);
 		},
 
-		onMessagesLoaded: function(aAll)
+		onMessagesLoaded: function FolderDisplayListenerManager_listener_onMessagesLoaded(aAll)
 		{
-			func.selectMessageDelayed();
+			FolderDisplayListenerManager_listener.selectMessageDelayed();
 		},
 
-		onMakeActive: function()
+		onMakeActive: function FolderDisplayListenerManager_listener_onMakeActive()
 		{
-			func.selectMessageDelayed();
+			FolderDisplayListenerManager_listener.selectMessageDelayed();
 		},
-	};
-	window.FolderDisplayListenerManager.registerListener(func);
-	listen(window, window, "unload", unload(function(){
-		if ("FolderDisplayListenerManager" in window)
-			window.FolderDisplayListenerManager.unregisterListener(func)
-		}), false);
-	func.selectMessage();
-}
+	}; //FolderDisplayListenerManager_listener
 	
-function disableAll(obj, r)
+	window.FolderDisplayListenerManager.registerListener(FolderDisplayListenerManager_listener);
+	
+	listen(window, window, "unload", unload(function()
+	{
+		if ("FolderDisplayListenerManager" in window)
+			window.FolderDisplayListenerManager.unregisterListener(FolderDisplayListenerManager_listener)
+	}), false);
+	
+	FolderDisplayListenerManager_listener.selectMessage();
+
+	let tabmail = document.getElementById("tabmail"),
+			tabMon = {
+		monitorName: "aslmTabmon",
+		onTabOpened: function tabMan_onTabOpened(tab, aFirstTab, aOldTab)
+		{
+			if (tab.mode.name != "preferencesTab")
+				return;
+
+			if (tab.browser.contentWindow.___autoSLM)
+				return;
+
+			tab.browser.addEventListener("paneSelected", function runOnce (event)
+			{
+				tab.browser.removeEventListener("paneSelected", runOnce, false);
+				fixpref(tab.browser.contentWindow);
+			}, false);
+			tab.browser.contentWindow.___autoSLM = true;
+			unload(function()
+			{
+				delete tab.browser.contentWindow.___autoSLM;
+			});
+
+		},
+		onTabTitleChanged: function tabMan_onTabTitleChanged(){},
+		onTabPersist: function tabMan_onTabPersist(){},
+		onTabRestored: function tabMan_onTabRestored(){},
+		onTabClosing: function tabMan_onTabClosing(){},
+		onTabSwitched: function tabMan_onTabSwitched(tab){},
+	};
+	
+	if (tabmail)
+	{
+		if (tabmail.tabTypes.preferencesTab)
+		{
+			for(let i = 0; i < tabmail.tabTypes.preferencesTab.modes.preferencesTab.tabs.length; i++)
+			{
+				fixpref(tabmail.tabTypes.preferencesTab.modes.preferencesTab.tabs[i].browser.contentWindow);
+			}
+		}
+		tabmail.registerTabMonitor(tabMon);
+		unload(function()
+		{
+			tabmail.unregisterTabMonitor(tabMon)
+		});
+	}
+
+} //main()
+
+function disableAll(obj, r, s)
 {
-	if (obj.tagName == "button" || obj.id.match(/^autoSLM/))
-		return;
+	if (!s && obj.hasAttribute && obj.hasAttribute("autoSLM"))
+		s = true;
 
-	if (!r && !("___autoSLM_disabled" in obj))
-		obj.___autoSLM_disabled = obj.disabled;
+	if (obj.tagName == "button" || (obj.hasAttribute && obj.hasAttribute("autoSLM")))
+		return true;
 
-	if (r && "___autoSLM_disabled" in obj)
-		obj.disabled = obj.___autoSLM_disabled;
-	else if (!r)
-		obj.disabled = true;
-
+	if (s || typeof(r) == "undefined")
+	{
+		if ("disabled" in obj && !("___autoSLM_disabled" in obj))
+		{
+			obj.___autoSLM_disabled = obj.disabled;
+		}
+		if (typeof(r) == "undefined")
+		{
+			obj.disabled = obj.___autoSLM_disabled;
+			delete obj.___autoSLM_disabled;
+		}
+		else
+		{
+			obj.disabled = r;
+		}
+	}
 	if (obj.childNodes.length)
 	{
-		for(var i = 0; i < obj.childNodes.length; i++)
+		for(let i = 0; i < obj.childNodes.length; i++)
 		{
-			disableAll(obj.childNodes[i],r);
+			let a = disableAll(obj.childNodes[i], r, s);
+			if (a)
+				s = a;
 		}
 	}
 }
@@ -237,19 +305,20 @@ function setDefaultPrefs(prefs, prefix)
 	}
 	return prefs;
 }
-
 function fixpref(window, r, s)
 {
-	var doc = window.document;
+	let doc = window.document;
 	if (!doc)
 		return;
 
 	r =  r ? true : false;
 
-	if (doc.getElementById("mailnewsStartPageEnabled"))
+	let startBox = doc.getElementById("mailnewsStartPageEnabled");
+	if (startBox)
 	{
 		function addElement(el, parent, type)
 		{
+			el.setAttribute("autoSLM", "");
 			type = type || "appendChild";
 
 			if (type == "insertBefore")
@@ -257,82 +326,112 @@ function fixpref(window, r, s)
 			else
 				parent[type](el);
 
-			if (!r)
+			listen(window, window, "unload", unload(function()
 			{
-				listen(window, window, "unload", unload(function()
-				{
-					el.parentNode.removeChild(el);
-				}), false);
-			}
+				el.parentNode.removeChild(el);
+			}), false);
+		}
+		function prefChange()
+		{
+			disableAll(startBox.parentNode.parentNode, pref.getIntPref("sel") ? true : false);
 		}
 		if (!r && !doc.getElementById("autoSLM_box"))
 		{
-			let h = doc.getElementById("mailnewsStartPageEnabled").parentNode.parentNode.clientHeight,
-					w = doc.getElementById("mailnewsStartPageEnabled").parentNode.parentNode.clientWidth,
-					checkbox = doc.createElement("checkbox"),
-					menupopup = doc.createElement("menupopup"),
-					menulist = doc.createElement("menulist"),
-					box = doc.createElement("hbox"),
-					menuitem = doc.createElement("menuitem"),
-					p = doc.createElement("preference");
+			let h = startBox.parentNode.parentNode.clientHeight,
+					w = startBox.parentNode.parentNode.clientWidth,
+					checkbox = doc.createXULElement("checkbox"),
+					menupopup = doc.createXULElement("menupopup"),
+					menulist = doc.createXULElement("menulist"),
+					box = doc.createXULElement("hbox"),
+					menuitem = doc.createXULElement("menuitem"),
+					p = doc.createXULElement("preference");
 
 			menulist.id = "autoSLM";
 			menulist.setAttribute("label", "Select first unread message only");
 			menulist.setAttribute("preference", PREF_BRANCH + "sel");
 			menulist.addEventListener("command", prefChange, true);
+			menulist.setAttribute("onsyncfrompreference", "prefChange(event)");
 			box.setAttribute("flex", false);
 			box.id = "autoSLM_box";
 			menuitem.setAttribute("value", 0);
 			menuitem.setAttribute("label", "Default");
 			menupopup.appendChild(menuitem);
-			menuitem = doc.createElement("menuitem");
+			menuitem = doc.createXULElement("menuitem");
 			menuitem.setAttribute("value", 1);
 			menuitem.setAttribute("label", "Newest message");
 			menupopup.appendChild(menuitem);
-			menuitem = doc.createElement("menuitem");
+			menuitem = doc.createXULElement("menuitem");
 			menuitem.setAttribute("value", 2);
 			menuitem.setAttribute("label", "First unread message");
 			menupopup.appendChild(menuitem);
 			menulist.appendChild(menupopup);
 			box.appendChild(menulist);
 
-			addElement(box, doc.getElementById("mailnewsStartPageEnabled").parentNode, "insertBefore");
+			addElement(box, startBox.parentNode, "insertBefore");
 
 			checkbox.id = "autoSLM_checkbox";
 			checkbox.setAttribute("label", "Auto focus on messages list");
 			checkbox.setAttribute("preference", PREF_BRANCH + "focus");
 			addElement(checkbox, box);
 
+			let ps = doc.getElementsByTagName("preferences");
+			if (ps.length)
+			{
+				ps = ps[0];
+			}
+			else
+			{
+				ps = doc.createXULElement("prefrences");
+				startBox.appendChild(ps);
+			}
+			addElement(p, ps);
+
 			p.id = PREF_BRANCH + "sel";
 			p.setAttribute("type", "int");
 			p.setAttribute("name", p.id);
-			addElement(p, doc.getElementById("generalPreferences"));
+			addElement(p, ps);
 
-			p = doc.createElement("preference");
+			p = doc.createXULElement("preference");
 			p.id = PREF_BRANCH + "focus";
 			p.setAttribute("type", "bool");
 			p.setAttribute("name", p.id);
-			addElement(p, doc.getElementById("generalPreferences"));
-			
-			disableAll(doc.getElementById("mailnewsStartPageEnabled").parentNode.parentNode, menulist.value == 0);
-			width = doc.getElementById("mailnewsStartPageEnabled").parentNode.parentNode.clientWidth - w;
-			height = doc.getElementById("mailnewsStartPageEnabled").parentNode.parentNode.clientHeight - h;
+			addElement(p, ps);
+
+			width = startBox.parentNode.parentNode.clientWidth - w;
+			height = startBox.parentNode.parentNode.clientHeight - h;
 //			window.resizeBy(width, height);
-			doc.getElementById("MailPreferences").showPane(doc.getElementById("MailPreferences").currentPane);
+			try
+			{
+				doc.getElementById("MailPreferences").showPane(doc.getElementById("MailPreferences").currentPane);
+			}
+			catch(e){}
+			try
+			{
+				let p = [
+					{ id: "extensions.autoselectlatestmessage.sel", type: "int" },
+					{ id: "extensions.autoselectlatestmessage.focus", type: "bool" },
+				];
+				window.Preferences.addAll(p);
+				unload(function()
+				{
+					for(let i = 0; i < p.length; i++)
+						delete window.Preferences._all[p[i].id];
+				});
+			}
+			catch(e){}
 		}
+		prefChange()
 		if (!r)
-			listen(window, window, "unload", unload(function(){
-				disableAll(doc.getElementById("mailnewsStartPageEnabled").parentNode.parentNode, true);
+			listen(window, window, "unload", unload(function()
+			{
+				disableAll(startBox.parentNode.parentNode);
 //				window.resizeBy(-width, -height);
 			}), false);
 	}
 	else if (!s && doc.getElementById("paneGeneral"))
-		doc.getElementById("paneGeneral").addEventListener("paneload", function() {fixpref(window, r, true)}, true);
-}
-function prefChange(e)
-{
-	disableAll(e.target.parentNode.parentNode.parentNode.parentNode, e.target.value == "0");
-}
+		listen(window, doc.getElementById("paneGeneral"), "paneload", function() {fixpref(window, r, true)}, true);
+
+} //fixpref()
 
 function startup(data, reason)
 {
